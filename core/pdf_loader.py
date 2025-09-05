45e7fe271eff9b1dab5810a0319ab8baa0b9593c
Debojyoti Banerjee <debojyotibanerjee2001@gmail.com>
2025-09-05 13:14:03 +0530

    Implemented code for pdf_loader.py

---
+++ core/pdf_loader.py
@@ -0,0 +1,19 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+class PDFLoaderManager:
+    def __init__(self,chunk_size=1000,chunk_overlap=200):
+        self.chunk_size=chunk_size
+        self.chunk_overlap=chunk_overlap
+    def load_pdf(self,file_paths:list):
+        all_docs=[]
+        for path in file_paths:
+            try:
+                loader=PyPDFLoader(path)
+                data=loader.load()
+                splitter=RecursiveCharacterTextSplitter(chunk_size=self.chunk_size,chunk_overlap=self.chunk_overlap)
+                split_docs=splitter.split_documents(data)
+                all_docs.append(split_docs)
+            except:
+                print("some error occured")
+        return all_docs
